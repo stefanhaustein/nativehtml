679a5d9b073e385c4bbf77040d639dffda28a3b9
stefan.haustein@gmail.com <stefan.haustein@gmail.com>
2017-10-03 23:24:17 +0200

    revert prefix map support

--- shared/src/main/java/org/kobjects/nativehtml/io/DefaultRequestHandler.java
+++ shared/src/main/java/org/kobjects/nativehtml/io/DefaultRequestHandler.java
@@ -3,8 +3,6 @@ package org.kobjects.nativehtml.io;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 import org.kobjects.nativehtml.dom.Platform;
@@ -12,7 +10,7 @@ import org.kobjects.nativehtml.dom.Platform;
 public class DefaultRequestHandler implements RequestHandler {
 
   private final Platform platform;
-  private TreeMap<String,String> internalLinkPrefixMap = new TreeMap<>();
+  private TreeSet<String> internalLinkPrefixSet = new TreeSet<>();
   private InternalLinkHandler internalLinkHandler;
   
   public DefaultRequestHandler(Platform platform) {
@@ -28,9 +26,9 @@ public class DefaultRequestHandler implements RequestHandler {
     if (internalLinkHandler != null) {
       String s = url.toString();
       // TODO: Take advantage of order and start scan just before s.
-      for (Map.Entry<String,String> entry : internalLinkPrefixMap.entrySet()) {
-        if (s.startsWith(entry.getKey())) {
-          openInternalLink(URI.create(entry.getValue() + s.substring(entry.getKey().length())));
+      for (String prefix : internalLinkPrefixSet) {
+        if (s.startsWith(prefix)) {
+          openInternalLink(url);
           return;
         }
       }
@@ -47,12 +45,9 @@ public class DefaultRequestHandler implements RequestHandler {
     }
   }
 
-  public void addInternalLinkPrefix(String prefix) {
-    internalLinkPrefixMap.put(prefix, prefix);
+  public void addInternalLinkPrefix(String s) {
+    internalLinkPrefixSet.add(s);
   }
 
-  public void addInternalLinkPrefix(String prefix, String replacement) {
-    internalLinkPrefixMap.put(prefix, replacement);
-  }
 
 }
