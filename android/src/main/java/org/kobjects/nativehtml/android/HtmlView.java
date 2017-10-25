582f29e6bd22cbd9a49656dd4a677deeccf86735
MartB <mart.b@outlook.de>
2017-10-25 19:21:18 +0200

    Add the required constructors for direct usage in layout xml files.
    Usage: <org.kobjects.nativehtml.android.HtmlView>

--- android/src/main/java/org/kobjects/nativehtml/android/HtmlView.java
+++ android/src/main/java/org/kobjects/nativehtml/android/HtmlView.java
@@ -1,22 +1,38 @@
 package org.kobjects.nativehtml.android;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.FrameLayout;
-import java.io.Reader;
-import java.net.URI;
+
 import org.kobjects.nativehtml.dom.Element;
 import org.kobjects.nativehtml.io.DefaultRequestHandler;
 import org.kobjects.nativehtml.io.HtmlParser;
 import org.kobjects.nativehtml.io.InternalLinkHandler;
 
+import java.io.Reader;
+import java.net.URI;
+
 public class HtmlView extends FrameLayout implements InternalLinkHandler {
-    private final AndroidPlatform platform;
-    private final HtmlParser htmlParser;
+    private HtmlParser htmlParser;
     private DefaultRequestHandler requestHandler;
 
     public HtmlView(Context context) {
         super(context);
-        platform = new AndroidPlatform(context);
+        init(context);
+    }
+
+    public HtmlView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public HtmlView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        AndroidPlatform platform = new AndroidPlatform(context);
         requestHandler = new DefaultRequestHandler(platform);
         requestHandler.setInternalLinkHandler(this);
         htmlParser = new HtmlParser(platform, requestHandler, null);
